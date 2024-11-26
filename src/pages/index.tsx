82c1ca02f20cdf795c404dec1d828de4e822e232
Emily Mikl <emilysmikl@gmail.com>
2024-11-26 13:49:15 -0600

    Update photos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,18 @@
 import * as React from "react"
 import PrimaryLayout from "../layouts/PrimaryLayout"
-import Grid from "@mui/material/Grid"
 import "../styles/home.scss"
 
 const Home = () => (
     <PrimaryLayout>
-        <Grid container>
-            <Grid item xs={12} lg={6}>
-                <div className="main-photo"></div>
-            </Grid>
-            <Grid item xs={12} lg={6}>
-                <div className="main-photo-2"></div>
-            </Grid>
-        </Grid>
-        <Grid container>
-            <Grid item xs={12} lg={4}>
-                <div className="secondary-photo-1"></div>
-            </Grid>
-            <Grid item xs={12} lg={4}>
-                <div className="secondary-photo-2"></div>
-            </Grid>
-            <Grid item xs={12} lg={4}>
-                <div className="secondary-photo-3"></div>
-            </Grid>
-        </Grid>
+        <div className="photo-grid">
+            <div className="main-photo"></div>
+            <div className="main-photo-2"></div>
+            <div className="secondary-photo-small"></div>
+            <div className="secondary-photo-large"></div>
+            <div className="third-row-1"></div>
+            <div className="third-row-2"></div>
+            <div className="third-row-3"></div>
+        </div>
     </PrimaryLayout>
 )
 
